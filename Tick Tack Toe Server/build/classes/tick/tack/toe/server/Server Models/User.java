44362fdbff6a6356379a4480f3c3bc65d5baf545
boogado@yahoo.com <boogado@yahoo.com>
2022-03-03 20:28:04 +0200

    "uploading server models"

---
+++ Tick Tack Toe Server/build/classes/tick/tack/toe/server/Server Models/User.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectjavamodels;
+
+/**
+ *
+ * @author booga
+ */
+public class User {
+    private int user_id;
+    private String name;
+    private int points;
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+    
+    
+    
+}
